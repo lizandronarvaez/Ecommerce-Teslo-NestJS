2d83bc5b0df29e32151bf01c81bb26ff64e43e7d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-09-08 16:22:20 +0200

    Aplicando para que un usuario este auntenticado si se cargan las semillas en la base de datos

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Authentication_Autorization } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  // @Authentication_Autorization()
   seedExecute() {
     return this.seedService.runSeed();
   }
