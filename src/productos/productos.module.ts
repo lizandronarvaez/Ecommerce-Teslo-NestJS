a557cd5818371ebb01e117028fa55b187616718d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-09-08 16:19:57 +0200

    Exportando el modulo de autenticacion al modulo de productos

--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -3,10 +3,11 @@ import { ProductosService } from './productos.service';
 import { ProductosController } from './productos.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto, ProductoImage } from './entities';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   // Cuando se crea una entity se debe agregarlo al modulo
-  imports: [TypeOrmModule.forFeature([Producto, ProductoImage])],
+  imports: [TypeOrmModule.forFeature([Producto, ProductoImage]), AuthModule],
   controllers: [ProductosController],
   providers: [ProductosService],
   exports: [ProductosService],
